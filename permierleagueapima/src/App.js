aac9544c3853ece98663a69f484dd1cd95df2de6
matiasgarciajohannesen <69842901+matiasgarciajohannesen@users.noreply.github.com>
2022-02-10 16:11:10 +0100

    adding teams and styling

--- permierleagueapima/src/App.js
+++ permierleagueapima/src/App.js
@@ -1,27 +1,37 @@
 
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-import { PREMTEAMS_URL, BASE_URL, POPULATE } from './utils/api';
+import {BASE_URL} from './utils/api';
+import './App.css';
+
 
 const App = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     axios
-      .get(BASE_URL + PREMTEAMS_URL + POPULATE)
+      .get(BASE_URL)
       .then((response) => setData(response.data.data));
+      
   }, []);
 
   return (
     <>
-      <h1>Hello World</h1>
-      <ul>
+      
+      <div className= "main">
+        <h1>Premier League Teams</h1>
         {data.length > 0
-          ? data.map((teams, idx) => {
-              return <li key={idx}>{teams.attributes.name}</li>;
+          ? data.map(({attributes, id}) => {
+              return (
+              <div className= "Team-Info">
+                <p key={id}>Team name: {attributes.team_name}</p>
+                <p key={id}>Stadium name: {attributes.stadium.data.attributes.stadium_name}</p>
+                <p key={id}>Stadium capacits: {attributes.stadium.data.attributes.Capacity}</p>
+              </div>)
+
             })
           : null}
-      </ul>
+      </div>
     </>
   );
 };
--- permierleagueapima/src/utils/api.js
+++ permierleagueapima/src/utils/api.js
@@ -1,5 +1,3 @@
 import axios from 'axios';
 
-export const BASE_URL = 'https://premierapi.herokuapp.com/api/premier-league-teams/';
-export const PREMTEAMS_URL = 'teams';
-export const POPULATE = '?populate=*';
+export const BASE_URL = 'https://premierapi.herokuapp.com/api/premier-league-teams/?populate=*';
