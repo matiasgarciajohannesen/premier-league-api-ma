ad86b15f42fe3c50bffe648245da37a4099be3bc
matiasgarciajohannesen <69842901+matiasgarciajohannesen@users.noreply.github.com>
2022-02-20 19:41:11 +0100

    styling

--- permierleagueapima/src/App.js
+++ permierleagueapima/src/App.js
@@ -20,18 +20,21 @@ const App = () => {
       
       <div className= "main">
         <h1>Premier League Teams 2021/2022</h1>
+        <div className= "page">
         {data.length > 0
           ? data.map(({attributes, id}) => {
               return (
-              <div className= "Team-Info">
-                <p key={id}>Team name: {attributes.team_name}</p>
-                <img key={id} src={attributes.img_url}/> 
-                <p key={id}>Stadium name: {attributes.stadium.data.attributes.stadium_name}</p>
-                <p key={id}>Stadium capacits: {attributes.stadium.data.attributes.Capacity}</p>
-              </div>)
+                <div className= "Team-Info">
+                  <p key={id}>Team name: {attributes.team_name}</p>
+                  <img key={id} src={attributes.img_url}/> 
+                  <p key={id}>Stadium name: {attributes.stadium.data.attributes.stadium_name}</p>
+                  <p key={id}>Stadium capacits: {attributes.stadium.data.attributes.Capacity}</p>
+                </div>
+              )
 
             })
           : null}
+          </div>
       </div>
     </>
   );
